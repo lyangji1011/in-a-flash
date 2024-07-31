df368ee5956fa3a99d2f5cd8f980b90f2cef383d
Lauren Ji <lyangji1011@gmail.com>
2024-07-31 12:42:39 -0400

    Implement set searching on dashbaord

--- src/components/dashboard/SetsDisplay.tsx
+++ src/components/dashboard/SetsDisplay.tsx
@@ -7,9 +7,17 @@ interface Props {
 
 export default function SetsDisplay({ sets }: Props) {
   return (
-    <div className="flex flex-row flex-wrap	justify-start gap-6">
-      {sets.map((set) =>
-        set.name ? <SetCard key={set.id} name={set.name} id={set.id} /> : null
+    <div>
+      {sets.length > 0 ? (
+        <div className="flex flex-row flex-wrap	justify-start gap-6">
+          {sets.map((set) =>
+            set.name ? (
+              <SetCard key={set.id} name={set.name} id={set.id} />
+            ) : null
+          )}
+        </div>
+      ) : (
+        <div className="ml-6">No sets match your search.</div>
       )}
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,14 @@ import SetsDisplay from "@/components/dashboard/SetsDisplay";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Input } from "@chakra-ui/react";
-import { Search2Icon } from "@chakra-ui/icons";
+import { SearchIcon } from "@chakra-ui/icons";
+import { FlashcardSet } from "@prisma/client";
 
 export default function Home() {
   const [user, setUser] = useState<User | undefined>(undefined);
   const [sets, setSets] = useState([]);
+  const [filteredSets, setFilteredSets] = useState([]);
+  const [filter, setFilter] = useState("");
   const router = useRouter();
 
   useEffect(() => {
@@ -41,6 +44,7 @@ export default function Home() {
         const data = await response.json();
         console.log(data);
         setSets(data.data);
+        setFilteredSets(data.data);
       } catch (e) {
         console.log(e);
       }
@@ -51,21 +55,55 @@ export default function Home() {
     }
   }, [user]);
 
+  const updateFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(event.target.value);
+  };
+
+  const applyFilter = () => {
+    console.log(filter);
+    if (filter.length === 0) {
+      setFilteredSets(sets);
+    } else {
+      setFilteredSets(
+        sets.filter((set: FlashcardSet) => {
+          if (set.name) {
+            return set.name?.toLowerCase().includes(filter.toLowerCase());
+          }
+          return false;
+        })
+      );
+    }
+  };
+
   return (
     <div>
       <Header name={user?.firstName} />
       {sets.length === 0 ? (
         <NoSets />
       ) : (
-        <div className="mx-[8vw] md:mx-[13vw] py-24">
+        <div className="mx-[8vw] md:mx-[13vw] py-28">
           <h1 className="text-5xl	font-poppins font-medium mb-3 font-sans">
             Your sets
           </h1>
           <div className="mb-10 flex flex-row items-center justify-start w-[350px] md:w-[400px] lg:w-[500px]">
-            <Input borderRadius={11} style={{ backgroundColor: "white" }} />
-            <Search2Icon boxSize={6} marginLeft="18px" />
+            <Input
+              value={filter}
+              borderRadius={11}
+              style={{ backgroundColor: "white" }}
+              onChange={updateFilter}
+            />
+            <SearchIcon
+              boxSize={6}
+              marginLeft="18px"
+              _hover={{
+                color: "#DC59A0",
+                cursor: "pointer",
+                transition: "color 0.2s ease",
+              }}
+              onClick={applyFilter}
+            />
           </div>
-          <SetsDisplay sets={sets} />
+          <SetsDisplay sets={filteredSets} />
         </div>
       )}
       <Footer />
