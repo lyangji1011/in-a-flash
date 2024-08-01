2dbeffc9f78d69f3679361fbc76b519c33c34176
Lauren Ji <lyangji1011@gmail.com>
2024-08-01 12:07:55 -0400

    Update readme and add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/inaflash-favicon.png"></link>
       </Head>
       <body>
         <Main />
